12f3a0aa0b49bc52f25d9d6918db735b7b806065
Shad DeHart <shaddehart509@gmail.com>
2021-12-31 14:16:43 -0500

    changed readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # rock-paper-scissors
+
 Rock paper scissors game where you play against the computer.
 
 What I learned
+
 - how to randomly select items out of an array
 - how to animate words (by splitting each individual character into their own span)
 - how to use .animate()
 - you can use a variable in place of document to select a query
+- learning how to fork and branch
+- learning git
--- app.js
+++ app.js
@@ -4,72 +4,72 @@ let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
 
-let buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body");
-const main = document.querySelector("main");
-const endAlrt = document.querySelector("#end-alert");
-const endDesc = document.querySelector("#end-desc");
-const returnMainBtn = document.querySelector("#retry-btn");
-const desc = document.querySelector("#desc3");
-const container = document.querySelector("#results-container");
+let buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
+const endAlrt = document.querySelector('#end-alert');
+const endDesc = document.querySelector('#end-desc');
+const returnMainBtn = document.querySelector('#retry-btn');
+const desc = document.querySelector('#desc3');
+const container = document.querySelector('#results-container');
 
-body.addEventListener("click", skipAnime());
-body.addEventListener("keydown", skipAnime());
+body.addEventListener('click', skipAnime());
+body.addEventListener('keydown', skipAnime());
 
 function skipAnime() {
-  const span = document.querySelectorAll("span");
+  const span = document.querySelectorAll('span');
 
-  span.forEach((span) => span.classList.add("skip"));
+  span.forEach((span) => span.classList.add('skip'));
 }
 
 function beginningAnimation() {
   fadeIn();
   //need to turn nodelist of spans into an array so we can access last value for ontransitionend
-  const desc1 = document.querySelector("#desc1");
-  let desc1Span = desc1.querySelectorAll("span");
+  const desc1 = document.querySelector('#desc1');
+  let desc1Span = desc1.querySelectorAll('span');
 
   desc1Span = Array.from(desc1Span);
 
-  const desc2 = document.querySelector("#desc2");
-  const desc3 = document.querySelector("#desc3");
+  const desc2 = document.querySelector('#desc2');
+  const desc3 = document.querySelector('#desc3');
 
   desc1Span[desc1Span.length - 1].ontransitionend = () => {
-    desc1.classList.add("fade-out");
+    desc1.classList.add('fade-out');
 
-    desc1.addEventListener("animationend", () => {
-      desc1.classList.add("disappear");
-      desc1.classList.remove("animate");
-      desc2.classList.remove("disappear");
-      desc2.classList.add("animate");
+    desc1.addEventListener('animationend', () => {
+      desc1.classList.add('disappear');
+      desc1.classList.remove('animate');
+      desc2.classList.remove('disappear');
+      desc2.classList.add('animate');
       fadeIn();
       /* need to collect nodelist of span 
 in the same function we activate fadein()
 or else nodelist will be empty */
-      let desc2Span = desc2.querySelectorAll("span");
+      let desc2Span = desc2.querySelectorAll('span');
       desc2Span = Array.from(desc2Span);
 
       desc2Span[desc2Span.length - 1].ontransitionend = () => {
-        desc2.classList.add("fade-out");
-        desc2.addEventListener("animationend", () => {
-          desc2.classList.add("disappear");
-          desc2.classList.remove("animate");
-          desc3.classList.remove("disappear");
-          desc3.classList.add("animate");
+        desc2.classList.add('fade-out');
+        desc2.addEventListener('animationend', () => {
+          desc2.classList.add('disappear');
+          desc2.classList.remove('animate');
+          desc3.classList.remove('disappear');
+          desc3.classList.add('animate');
           fadeIn();
 
-          let desc3Span = desc3.querySelectorAll("span");
+          let desc3Span = desc3.querySelectorAll('span');
           desc3Span = Array.from(desc3Span);
 
           desc3Span[desc3Span.length - 1].ontransitionend = () => {
-            const cta = document.querySelector("#cta");
+            const cta = document.querySelector('#cta');
 
-            cta.classList.add("drop-down");
+            cta.classList.add('drop-down');
 
-            cta.addEventListener("animationend", () => {
-              const gameCtn = document.querySelector("#game-container");
+            cta.addEventListener('animationend', () => {
+              const gameCtn = document.querySelector('#game-container');
 
               setTimeout(function () {
-                gameCtn.classList.add("fade-in");
+                gameCtn.classList.add('fade-in');
               }, 300);
             });
           };
@@ -79,11 +79,11 @@ or else nodelist will be empty */
   };
 }
 function fadeIn() {
-  let text = document.querySelector(".animate");
+  let text = document.querySelector('.animate');
 
   let strText = text.textContent;
-  let splitText = strText.split("");
-  text.textContent = "";
+  let splitText = strText.split('');
+  text.textContent = '';
   //append span tags to each character in the string
   for (i = 0; i < splitText.length; i++) {
     text.innerHTML += `<span>${splitText[i]}</span>`;
@@ -93,8 +93,8 @@ function fadeIn() {
   let timer = setInterval(onTick, 50);
 
   function onTick() {
-    const span = text.querySelectorAll("span")[char];
-    span.classList.add("fade");
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
     char++;
     //stops the function from running once the end of the string has been reached
     if (char === splitText.length) {
@@ -109,8 +109,8 @@ function fadeIn() {
 }
 
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const img = button.querySelector("img");
+  button.addEventListener('click', () => {
+    const img = button.querySelector('img');
     playerSelection = img.alt.toLowerCase();
 
     playRound(playerSelection, computerSelection);
@@ -121,7 +121,7 @@ buttons.forEach((button) => {
   });
 });
 
-const myArray = ["Rock", "Paper", "Scissors"];
+const myArray = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
   return myArray[~~(Math.random() * myArray.length)];
@@ -131,11 +131,11 @@ function playRound(playerSelection, computerSelection) {
   computerSelection = computerPlay().toLowerCase();
   playerSelection = playerSelection.toLowerCase();
   if (computerSelection == playerSelection) {
-    displayResults("Tie game!");
+    displayResults('Tie game!');
   } else if (
-    (computerSelection == "rock" && playerSelection == "scissors") ||
-    (computerSelection == "scissors" && playerSelection == "paper") ||
-    (computerSelection == "paper" && playerSelection == "rock")
+    (computerSelection == 'rock' && playerSelection == 'scissors') ||
+    (computerSelection == 'scissors' && playerSelection == 'paper') ||
+    (computerSelection == 'paper' && playerSelection == 'rock')
   ) {
     computerScore = ++computerScore;
     keepCpuScore();
@@ -204,10 +204,10 @@ function capitalize(str) {
 function displayResults(str) {
   container.animate([{ opacity: 0 }, { opacity: 1 }], {
     duration: 300,
-    fill: "forwards",
+    fill: 'forwards',
     iterations: 1,
     delay: 0,
-    easing: "ease-out",
+    easing: 'ease-out',
   });
   container.textContent = str;
 }
@@ -215,19 +215,19 @@ function displayResults(str) {
 function declareWinner() {
   rplContent();
   if (playerScore > computerScore) {
-    endDesc.textContent = "You win! Mankind lives another day!!";
-    returnMainBtn.innerText = "Play Again";
+    endDesc.textContent = 'You win! Mankind lives another day!!';
+    returnMainBtn.innerText = 'Play Again';
   } else {
-    endDesc.textContent = "You lost...who will save mankind now?";
-    returnMainBtn.innerText = "Try Again?";
+    endDesc.textContent = 'You lost...who will save mankind now?';
+    returnMainBtn.innerText = 'Try Again?';
   }
   fadeIn();
 
-  let endDescSpan = endDesc.querySelectorAll("span");
+  let endDescSpan = endDesc.querySelectorAll('span');
   endDescSpan = Array.from(endDescSpan);
 
   endDescSpan[endDescSpan.length - 1].ontransitionend = () => {
-    returnMainBtn.classList.add("fade-in");
+    returnMainBtn.classList.add('fade-in');
     /*returnMainBtn.animate([{ opacity: 0 }, { opacity: 1 }], {
       duration: 00,
       fill: "forwards",
@@ -239,23 +239,23 @@ function declareWinner() {
 }
 
 function rplContent() {
-  main.classList.add("disappear");
-  endAlrt.classList.remove("disappear");
-  desc.classList.remove("animate");
-  endDesc.classList.add("animate");
+  main.classList.add('disappear');
+  endAlrt.classList.remove('disappear');
+  desc.classList.remove('animate');
+  endDesc.classList.add('animate');
 
-  returnMainBtn.addEventListener("click", () => {
-    main.classList.remove("disappear");
-    endAlrt.classList.add("disappear");
-    desc.classList.add("animate");
-    returnMainBtn.classList.remove("fade-in");
+  returnMainBtn.addEventListener('click', () => {
+    main.classList.remove('disappear');
+    endAlrt.classList.add('disappear');
+    desc.classList.add('animate');
+    returnMainBtn.classList.remove('fade-in');
     resetGame();
   });
 }
 
 function resetGame() {
   fadeIn();
-  container.textContent = "";
+  container.textContent = '';
   playerScore = 0;
   computerScore = 0;
   keepPlayerScore();
@@ -263,28 +263,29 @@ function resetGame() {
 }
 
 function keepPlayerScore() {
-  let playerScoreBox = document.querySelector("#player-score");
+  let playerScoreBox = document.querySelector('#player-score');
 
   playerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
     duration: 300,
-    fill: "forwards",
+    fill: 'forwards',
     iterations: 1,
     delay: 0,
-    easing: "ease-out",
+    easing: 'ease-out',
   });
 
   playerScoreBox.textContent = playerScore;
 }
 function keepCpuScore() {
-  let computerScoreBox = document.querySelector("#computer-score");
+  let computerScoreBox = document.querySelector('#computer-score');
 
   computerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
     duration: 300,
-    fill: "forwards",
+    fill: 'forwards',
     iterations: 1,
     delay: 0,
-    easing: "ease-out",
+    easing: 'ease-out',
   });
 
   computerScoreBox.textContent = computerScore;
 }
+// add this line to see change in github
